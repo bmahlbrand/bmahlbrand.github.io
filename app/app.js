dc8e2700d6c7f34a85de3cd92d846a06b246b13e
Ben Ahlbrand <bmahlbrand@gmail.com>
2017-02-05 07:32:40 -0500

    tweak

--- app/app.js
+++ app/app.js
@@ -8,11 +8,5 @@ import './js/research/research';
 import './js/projects/projects';
 import './js/blog/blog';
 import './js/gallery/gallery';
-
-app.config(($stateProvider) => {
-    $stateProvider.state('resume', {
-        url: '/resume'
-    });
-});
 // app.run(['$rootScope', function($scope) {
 // }]);
