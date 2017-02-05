d91f49b3312673c82bb435a57f2d71625b93603d
Ben Ahlbrand <bmahlbrand@gmail.com>
2017-02-05 07:28:14 -0500

    resume update

--- app/app.js
+++ app/app.js
@@ -8,5 +8,11 @@ import './js/research/research';
 import './js/projects/projects';
 import './js/blog/blog';
 import './js/gallery/gallery';
+
+app.config(($stateProvider) => {
+    $stateProvider.state('resume', {
+        url: '/resume'
+    });
+});
 // app.run(['$rootScope', function($scope) {
 // }]);
