28613dd5106271225a2cc3741333ce1d21a95721
Ben Ahlbrand <bmahlbrand@gmail.com>
2017-02-10 03:26:03 -0500

    add webgl sandbox

--- app/app.js
+++ app/app.js
@@ -8,5 +8,7 @@ import './js/research/research';
 import './js/projects/projects';
 import './js/blog/blog';
 import './js/gallery/gallery';
+import './js/sandbox/sandbox';
+
 // app.run(['$rootScope', function($scope) {
 // }]);
---
+++ app/js/sandbox/sandbox.ctrl.js
@@ -0,0 +1,9 @@
+function SandboxController ($scope) {
+
+    $scope.message = 'I created this site with Node, Express, Angular, Bootstrap';
+
+    // $scope.templateUrl = "js/src/projects/index.html"
+
+};
+
+export default ['$scope', SandboxController];
