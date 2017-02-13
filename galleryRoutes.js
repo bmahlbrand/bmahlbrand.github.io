79b31c370490e0ac3f4a60e8dd48d56bec52f7ea
Ben Ahlbrand <bmahlbrand@gmail.com>
2017-02-13 06:22:57 -0500

    clean output

--- galleryRoutes.js
+++ galleryRoutes.js
@@ -8,8 +8,6 @@ const galleryRoutes = module.exports = express();
 galleryRoutes.get('*pix', (req, res) => {
     Gallery.find({}, (err, gallery) => {
         if (!err) {
-            console.log(gallery);
-            console.log('fire');
             res.json(gallery);
         } else {
             console.log(err);
--- projectsRoutes.js
+++ projectsRoutes.js
@@ -5,8 +5,6 @@ const projectsRoutes = module.exports = express();
 projectsRoutes.get('/projects/', (req, res) => {
     Project.find({}, (err, projects) => {
         if (!err) {
-            console.log(projects);
-            console.log('fire');
             res.json(projects);
         } else {
             console.log(err);
