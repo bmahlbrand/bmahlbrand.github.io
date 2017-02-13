f9a47cecd3b9b7ca2e850c8ef5a12c07ffff8ef0
Ben Ahlbrand <bmahlbrand@gmail.com>
2017-02-13 06:30:03 -0500

    missed this in output cleaning

--- blogPostRoutes.js
+++ blogPostRoutes.js
@@ -7,8 +7,6 @@ const blogRoutes = module.exports = express();
 blogRoutes.get('/data/posts/', (req, res) => {
     BlogPost.find({}, (err, posts) => {
         if (!err) {
-            console.log(posts);
-            console.log('fire');
             res.json(posts);
         } else {
             console.log(err);
