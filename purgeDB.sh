2c33222510f436de9bc95c7507815c32cb1ce0d2
Ben Ahlbrand <bmahlbrand@gmail.com>
2017-02-14 15:13:34 -0500

    fix shell script

--- purgeDB.sh
+++ purgeDB.sh
@@ -2,7 +2,7 @@ mongo blog --eval "db.projects.drop()"
 mongoimport -d blog -c projects --file app/js/projects/projects.json --jsonArray
 
 mongo blog --eval "db.posts.drop()"
-mongoimport -d blog -c posts --file app/js/posts/posts.json --jsonArray
+mongoimport -d blog -c posts --file app/js/blog/posts.json --jsonArray
 
 mongo blog --eval "db.gallery.drop()"
-mongoimport -d blog -c gallery --file app/js/gallery/gallery.json --jsonArray
+mongoimport -d blog -c gallery --file app/js/gallery/pix.json --jsonArray
