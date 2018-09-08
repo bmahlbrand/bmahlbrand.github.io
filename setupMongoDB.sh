ff0809b0b047270faeb9511809cfc8a5e98c5afc
Ben Ahlbrand <bmahlbrand@gmail.com>
2018-09-08 05:42:16 -0400

    Update setupMongoDB.sh

--- setupMongoDB.sh
+++ setupMongoDB.sh
@@ -1,10 +1,12 @@
 #mongodb
+
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 sudo apt-get update
 sudo apt-get install -y mongodb
 
+rm /etc/systemd/system/mongodb.service
 touch /etc/systemd/system/mongodb.service
 
 
